798663b0d11236d3b6b0dcb2252d1bb741d8329d
nykh2010 <xulingfeng@boe.com.cn>
2019-06-18 15:02:09 +0800

    settime.html time interval

--- gwweb/static/js/gateway.js
+++ gwweb/static/js/gateway.js
@@ -2,33 +2,5 @@ $(function(){
     $(".return_a").click(function(){
         window.history.back();
     });
-
-    //var timer = setInterval(refresh_sys_time, 1000);
-
-    function checkTime(i) {
-        if (i<10) {
-            i="0" + i;
-        }
-        return i;
-    }
-    
-    function refresh_sys_time() {
-        var time = new Date();
-        // 2019-01-21 10:47:56
-        year = time.getFullYear();
-        month = time.getMonth()+1;
-        days = time.getDate();
-        hours = time.getHours();
-        minutes = time.getMinutes();
-        seconds = time.getSeconds();
-        month = checkTime(month);
-        days = checkTime(days);
-        hours = checkTime(hours);
-        minutes = checkTime(minutes);
-        seconds = checkTime(seconds);
-        var strtime = year+"-"+month+"-"+days+"&nbsp"+hours+":"+minutes+":"+seconds;
-        $("#current_time").html(strtime);
-        delete time;
-    }
 });
 
